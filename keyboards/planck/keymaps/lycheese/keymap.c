c55da66140edacf57130623339e222a378e9fb5b
Elia Nolz <dev@lycheese.org>
2021-10-06 12:40:23 +0200

    Make the kyria thumb keys symmetrical

--- keyboards/planck/keymaps/lycheese/keymap.c
+++ keyboards/planck/keymaps/lycheese/keymap.c
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RALT_SH, KC_Q,            KC_W,             KC_E,         KC_R,            KC_T,    KC_Y,    KC_U,            KC_I,            KC_O,            KC_P,            KC_LBRC,
     KC_ESC,  LGUI_T(KC_A),    LALT_T(KC_S),     LCTL_T(KC_D), LSFT_T(KC_F),    KC_G,    KC_H,    RSFT_T(KC_J),    RCTL_T(KC_K),    LALT_T(KC_L),    RGUI_T(KC_SCLN), KC_QUOT,
     KC_LSFT, KC_Z,            KC_X,             KC_C,         KC_V,            KC_B,    KC_N,    KC_M,            KC_COMM,         KC_DOT,          KC_SLSH,         KC_RSFT,
-    XXXXXXX, KC_LEFT,         KC_RIGHT,         KC_NUBS,      KC_CAPS,         KC_SPC,  KC_SPC,  KC_RALT,         KC_BSLS,         KC_DOWN,         KC_UP,           XXXXXXX
+    XXXXXXX, KC_LEFT,         KC_RIGHT,         KC_CAPS,      KC_NUBS,         KC_SPC,  KC_SPC,  KC_RALT,         KC_BSLS,         KC_DOWN,         KC_UP,           XXXXXXX
 ),
 
 [_RALT_SHIFT] = LAYOUT_planck_grid(
