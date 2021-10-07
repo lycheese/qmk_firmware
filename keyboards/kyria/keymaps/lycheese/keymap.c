0008072e0a89eb4d15b18249da5243a357dda43f
Elia Nolz <dev@lycheese.org>
2021-10-07 10:14:58 +0200

    Put mod4 back as a mod-tap under space

--- keyboards/kyria/keymaps/lycheese/keymap.c
+++ keyboards/kyria/keymaps/lycheese/keymap.c
@@ -43,10 +43,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-      MO(_ADJUST),          KC_Q,           KC_W,           KC_E,            KC_R,           KC_T,                                                                                         KC_Y,    KC_U,         KC_I,         KC_O,         KC_P,            KC_LBRC,
-      KC_ESC            ,   LGUI_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_D),    LSFT_T(KC_F),   KC_G,                                                                                         KC_H,    RSFT_T(KC_J), RCTL_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
-      KC_LSFT           ,   KC_Z,           KC_X,           KC_C,            KC_V,           KC_B,           TG(_LOWER),     KC_SLCK,                                KC_LSFT, TG(_PLOVER), KC_N,    KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_RSFT,
-                                                            MO(_RAISE),      XXXXXXX,        KC_CAPS,        KC_SPC,         KC_NUBS,                                KC_RALT, KC_SPC,      KC_BSLS, XXXXXXX,      MO(_RAISE)
+      MO(_ADJUST),          KC_Q,           KC_W,           KC_E,            KC_R,           KC_T,                                                                                            KC_Y,    KC_U,         KC_I,         KC_O,         KC_P,            KC_LBRC,
+      KC_ESC            ,   LGUI_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_D),    LSFT_T(KC_F),   KC_G,                                                                                            KC_H,    RSFT_T(KC_J), RCTL_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
+      KC_LSFT           ,   KC_Z,           KC_X,           KC_C,            KC_V,           KC_B,           TG(_LOWER),     KC_SLCK,                                KC_LSFT, TG(_PLOVER),    KC_N,    KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_RSFT,
+                                                            MO(_RAISE),      XXXXXXX,        KC_CAPS,        RALT_T(KC_SPC), XXXXXXX,                                XXXXXXX, RALT_T(KC_SPC), KC_BSLS, XXXXXXX,      MO(_RAISE)
     ),
 /*
  * Lower Layer: Gaming
